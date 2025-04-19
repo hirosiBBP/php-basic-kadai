36044487407584192e9a94f368931061e320ce7b
hirosiBBP <rtf53942@nifty.com>
2025-04-19 14:51:31 +0900

    Add files via upload

---
+++ kadai_011/foreach.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <p>
+        <?php
+           $personal_data = ['name' => '玉ねぎ', 'price' => 200, 'production area' => '北海道'];
+           $key_translation = ['name' => '名前', 'price' => '値段', 'production area' => '産地'];
+        
+           foreach ($personal_data as $key => $value) {
+           $japanese_key = $key_translation[$key];
+           echo "{$japanese_key}:{$value}<br>";
+           }
+        ?>
+    </p>
+</body>
+
+</html>
